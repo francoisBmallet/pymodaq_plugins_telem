388b3a0a03181c880b2da694aa9a1f53cb3037b1
Francois Mallet <francois.mallet@sorbonne-universite.fr>
2025-12-04 14:05:00 +0100

    correction viewer multi agilent

--- src/pymodaq_plugins_telem/daq_viewer_plugins/plugins_0D/daq_0Dviewer_Multi_Agilent34ASerie.py
+++ src/pymodaq_plugins_telem/daq_viewer_plugins/plugins_0D/daq_0Dviewer_Multi_Agilent34ASerie.py
@@ -10,14 +10,18 @@ from pymodaq.control_modules.viewer_utility_classes import (
     comon_parameters,
     main
 )
-from pymodaq.utils.parameter import Parameter, utils
+from pymodaq.utils.parameter import Parameter
 from pymodaq.utils.data import DataFromPlugins, DataToExport
 
-from pyqtgraph.parametertree.Parameter import registerParameterType
-from pyqtgraph.parametertree.parameterTypes.basetypes import GroupParameter
-
 from pymodaq_plugins_telem.hardware.agilent_34A_thread_safe import Agilent34AThreadSafe
+
+
+# -----------------------------------------------------------------------------
+# CHANNEL DEFINITIONS
+# -----------------------------------------------------------------------------
+
 CHANNELS = ['voltage', 'voltage_ac', 'current', 'current_ac', 'resistance', 'resistance_4w']
+
 MODE_MAP = {
     "voltage": "voltage",
     "voltage_ac": "ac voltage",
@@ -26,92 +30,78 @@ MODE_MAP = {
     "resistance": "resistance",
     "resistance_4w": "4w resistance",
 }
+
+UNIT_MAP = {
+    "voltage": "V",
+    "voltage_ac": "V",
+    "current": "A",
+    "current_ac": "A",
+    "resistance": "Ω",
+    "resistance_4w": "Ω",
+}
+
+
+# -----------------------------------------------------------------------------
+# VISA
+# -----------------------------------------------------------------------------
+
 rm = pyvisa.ResourceManager()
 VISA_RESOURCES = rm.list_resources()
-ADAPTERS = dict(VISA=VISAAdapter, Prologix=PrologixAdapter)
+
+ADAPTERS = dict(
+    VISA=VISAAdapter,
+    Prologix=PrologixAdapter
+)
 
 
-for channel in CHANNELS:
-    assert hasattr(Agilent34AThreadSafe, channel)
-
-class ChannelGroup(GroupParameter):
-    """Group Parameter listing the different output
-    """
-
-    def __init__(self, **opts) -> None:
-        opts['type'] = 'agilent34Achannel'
-        opts['addText'] = "Add Channel"
-        super().__init__(**opts)
-
-    def addNew(self) -> None:
-        """Add new channel to viewer
-        """
-        name_prefix = 'Agilent34ACh'
-
-        child_indexes = [int(par.name()[len(name_prefix) + 1:])
-                         for par in self.children()]
-
-        if child_indexes == []:
-            newindex = 0
-        else:
-            newindex = max(child_indexes) + 1
-
-        child = {
-            'title': f'Measure {newindex:02.0f}',
-            'name': f'{name_prefix}{newindex:02.0f}',
-            'type': 'itemselect',
-            'removable': True,
-            # FIX → selected must be a list, NOT a string
-            'value': dict(all_items=CHANNELS, selected=[CHANNELS[0]])
-        }
-
-        self.addChild(child)
-
-
-registerParameterType('agilent34Achannel', ChannelGroup, override=True)
-
+# -----------------------------------------------------------------------------
+# MAIN PLUGIN CLASS
+# -----------------------------------------------------------------------------
 
 class DAQ_0DViewer_Multi_Agilent34ASerie(DAQ_Viewer_base):
-    """ Instrument plugin class for a OD viewer.
-
-    This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Viewer module through inheritance via
-    DAQ_Viewer_base. It makes a bridge between the DAQ_Viewer module and the Python wrapper of a particular instrument.
-
-    Attributes:
-    -----------
-    controller: object
-        The particular object that allow the communication with the hardware, in general a python wrapper around the
-         hardware library.
-
-    """
+    """Single-channel Agilent 34A PyMoDAQ Viewer with unit display"""
 
     params = [
-                 {'title': 'Adapter', 'name': 'adapter', 'type': 'list',
-                  'limits': list(ADAPTERS.keys())},
-                 {'title': 'VISA Address:', 'name': 'address', 'type': 'list',
-                  'limits': VISA_RESOURCES},
-                 {'title': 'ID:', 'name': 'id', 'type': 'str'},
-                 {'title': 'Channels:', 'name': 'channels', 'type': 'agilent34Achannel'}
-             ] + comon_parameters + [
-                {'title': 'Configuration:', 'name': 'config', 'type': 'group', 'children': [
-                {'title': 'Reset:', 'name': 'reset', 'type': 'bool_push', 'value': False},
-                {'title': 'Setup number:', 'name': 'setup_number', 'type': 'int', 'value': 1, 'min': 1, 'max': 9},
-                {'title': 'Save setup:', 'name': 'save_setup', 'type': 'bool_push', 'value': False,'label': 'Save'},
-                {'title': 'Load setup:', 'name': 'load_setup', 'type': 'bool_push', 'value': False,'label': 'Load'}
-                ]},
+        {'title': 'Adapter', 'name': 'adapter', 'type': 'list',
+         'limits': list(ADAPTERS.keys())},
+
+        {'title': 'VISA Address:', 'name': 'address', 'type': 'list',
+         'limits': VISA_RESOURCES},
+
+        {'title': 'ID:', 'name': 'id', 'type': 'str'},
+
+        # ✅ SINGLE CHANNEL SELECTION
+        {'title': 'Channel:', 'name': 'channel', 'type': 'list',
+         'limits': CHANNELS},
+
+        # ✅ UNIT DISPLAY (READ ONLY)
+        {'title': 'Unit:', 'name': 'unit', 'type': 'str', 'readonly': True},
+
+    ] + comon_parameters + [
+
+        {'title': 'Configuration:', 'name': 'config', 'type': 'group', 'children': [
+            {'title': 'Reset:', 'name': 'reset', 'type': 'bool_push', 'value': False},
+            {'title': 'Setup number:', 'name': 'setup_number', 'type': 'int', 'value': 1, 'min': 1, 'max': 9},
+            {'title': 'Save setup:', 'name': 'save_setup', 'type': 'bool_push', 'value': False, 'label': 'Save'},
+            {'title': 'Load setup:', 'name': 'load_setup', 'type': 'bool_push', 'value': False, 'label': 'Load'}
+        ]},
     ]
+
+
+    # -------------------------------------------------------------------------
+    # INIT
+    # -------------------------------------------------------------------------
 
     def ini_attributes(self):
         self.controller: Agilent34AThreadSafe = None
 
-    def commit_settings(self, param: Parameter):
-        """Apply the consequences of a change of value in the detector settings
 
-        Parameters
-        ----------
-        param: Parameter
-            A given parameter (within detector_settings) whose value has been changed by the user
-        """
+    # -------------------------------------------------------------------------
+    # PARAMETER CHANGES
+    # -------------------------------------------------------------------------
+
+    def commit_settings(self, param: Parameter):
+
         if param.name() == 'load_setup':
             self.controller.load_setup(self.settings['config', 'setup_number'])
             param.setValue(False)
@@ -120,67 +110,49 @@ class DAQ_0DViewer_Multi_Agilent34ASerie(DAQ_Viewer_base):
             self.controller.save_setup(self.settings['config', 'setup_number'])
             param.setValue(False)
 
-        elif param.name() in utils.iter_children(
-
-                self.settings.child('channels'), []):
-
-            data = []
-
-            for child in self.settings.child('channels').children():
-                labels = child.value()['selected']
-
-                data.append(
-
-                    DataFromPlugins(
-
-                        name=child.name(),
-
-                        data=[np.array([0]) for _ in labels],
-
-                        labels=labels,
-
-                        dim='Data0D'
-
-                    )
-
-                )
-
-            self.dte_signal_temp.emit(DataToExport(
-
-                name="agilent34A",
-
-                data=data
-
-            ))
-
         elif param.name() == 'reset':
             self.controller.reset()
             param.setValue(False)
 
+        # ✅ UPDATE UNIT WHEN CHANNEL CHANGES
+        elif param.name() == 'channel':
+            label = self.settings['channel']
+            unit = UNIT_MAP.get(label, "")
+            self.settings.child('unit').setValue(unit)
+
+            # Update temp data structure
+            self.dte_signal_temp.emit(
+                DataToExport(
+                    name="agilent34A",
+                    data=[
+                        DataFromPlugins(
+                            name="agilent34A",
+                            data=[np.array([0])],
+                            labels=[label],
+                            dim="Data0D"
+                        )
+                    ]
+                )
+            )
+
+
+    # -------------------------------------------------------------------------
+    # INITIALIZATION
+    # -------------------------------------------------------------------------
 
     def ini_detector(self, controller=None):
-        """Detector communication initialization
-
-        Parameters
-        ----------
-        controller: (object)
-            custom object of a PyMoDAQ plugin (Slave case). None if only one actuator/detector by controller
-            (Master case)
-
-        Returns
-        -------
-        info: str
-        initialized: bool
-            False if initialization failed otherwise True
-        """
 
         self.ini_detector_init(slave_controller=controller)
 
         if self.is_master:
-            adapter = ADAPTERS[self.settings.child('adapter').value()](
-                self.settings.child('address').value()
+            adapter = ADAPTERS[self.settings['adapter']](
+                self.settings['address']
             )
             self.controller = Agilent34AThreadSafe(adapter)
+
+        # Initialize unit display
+        label = self.settings['channel']
+        self.settings.child('unit').setValue(UNIT_MAP.get(label, ""))
 
         self.dte_signal_temp.emit(
             DataToExport(
@@ -189,8 +161,8 @@ class DAQ_0DViewer_Multi_Agilent34ASerie(DAQ_Viewer_base):
                     DataFromPlugins(
                         name="agilent34A",
                         data=[np.array([0])],
-                        dim="Data0D",
-                        labels=["x"]
+                        labels=[label],
+                        dim="Data0D"
                     )
                 ]
             )
@@ -205,46 +177,58 @@ class DAQ_0DViewer_Multi_Agilent34ASerie(DAQ_Viewer_base):
 
         return info, initialized
 
+
+    # -------------------------------------------------------------------------
+    # DATA ACQUISITION
+    # -------------------------------------------------------------------------
+
+    def grab_data(self, Naverage=1, **kwargs):
+
+        label = self.settings['channel']
+        mode = MODE_MAP[label]
+
+        # ✅ SAFE MODE SWITCHING
+        if self.controller.mode != mode:
+            self.controller.mode = mode
+
+        value = getattr(self.controller, label)
+
+        self.dte_signal.emit(
+            DataToExport(
+                name="agilent34A",
+                data=[
+                    DataFromPlugins(
+                        name="agilent34A",
+                        data=[np.array([value])],
+                        labels=[label],
+                        dim="Data0D"
+                    )
+                ]
+            )
+        )
+
+
+    # -------------------------------------------------------------------------
+    # STOP
+    # -------------------------------------------------------------------------
+
+    def stop(self):
+        return ''
+
+
+    # -------------------------------------------------------------------------
+    # CLOSE
+    # -------------------------------------------------------------------------
+
     def close(self):
-        """Terminate the communication protocol"""
         if self.controller is not None:
             self.controller.clear()
             self.controller.shutdown()
 
-    def grab_data(self, Naverage=1, **kwargs):
-        data = []
 
-        for child in self.settings.child('channels').children():
-            labels = child.value()['selected'][:]
-
-            subdata = []
-
-            for label in labels:
-                mode = MODE_MAP[label]
-
-                # Only change mode if it is different
-                if getattr(self.controller, "mode") != mode:
-                    self.controller.mode = mode
-
-                value = getattr(self.controller, label)  # thread-safe wrapper
-                subdata.append(np.array([value]))
-
-            data.append(DataFromPlugins(
-                name=child.name(),
-                data=subdata,
-                labels=labels,
-                dim='Data0D'
-            ))
-
-        self.dte_signal.emit(DataToExport(
-            name="agilent34A",
-            data=data
-        ))
-
-    def stop(self):
-        """Stop the current grab hardware wise if necessary"""
-        return ''
-
+# -----------------------------------------------------------------------------
+# MAIN
+# -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     main(__file__, init=False)
